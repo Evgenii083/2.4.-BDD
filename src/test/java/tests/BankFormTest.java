285eda7b52163073f81f79467d599a34d3ca8640
Evgenii Rybalkin <eugeny.rybalkin@gmail.com>
2023-02-02 00:38:05 +0100

    change tests for catching bugs

--- src/test/java/tests/BankFormTest.java
+++ src/test/java/tests/BankFormTest.java
@@ -12,7 +12,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class BankFormTest {
     @BeforeEach
-    public void SetUp() {
+    public void setUp() {
         open("http://localhost:9999/");
     }
 
@@ -44,6 +44,27 @@ public class BankFormTest {
     }
 
     @Test
+    public void shouldTransferMoneyFromCardToCardAndCompereBalance() {
+        var LoginPage = new LoginPage();
+        var DashboardPage = new DashboardPage();
+        var ReplenishPage = new ReplenishPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = LoginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCode(authInfo);
+        verificationPage.validVerify(verificationCode);
+        var balance = DashboardPage.getBalanceForCard(0);
+        DashboardPage.addTo(0);
+        ReplenishPage.moneyTransfer(2500, 1);
+
+        DashboardPage.addTo(1);
+        ReplenishPage.moneyTransfer(2500, 0);
+        var secondBalance = DashboardPage.getBalanceForCard(0);
+
+        System.out.println(balance + "\n" + secondBalance);
+        Assertions.assertEquals(balance, secondBalance);
+    }
+
+    @Test
     public void shouldNotMakeMoneyTransferIfTheTransferAmountIsTooMuch() {
         var LoginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
@@ -54,24 +75,8 @@ public class BankFormTest {
         var ReplenishPage = new ReplenishPage();
         DashboardPage.addTo(1);
         ReplenishPage.moneyTransfer(500_000, 0);
+        System.out.println(DashboardPage.getBalanceForCard(0));
+        Assertions.assertTrue(DashboardPage.getBalanceForCard(0) >= 0);
     }
 
-    @Test
-    public void shouldTransferMoneyFromCardToCardAndCompereBalance() {
-        var LoginPage = new LoginPage();
-        var DashboardPage = new DashboardPage();
-        var ReplenishPage = new ReplenishPage();
-        var authInfo = DataHelper.getAuthInfo();
-        var verificationPage = LoginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCode(authInfo);
-        verificationPage.validVerify(verificationCode);
-        var firstBalance = DashboardPage.getBalanceForCard(0);
-        DashboardPage.addTo(0);
-        ReplenishPage.moneyTransfer(5000, 1);
-        var secondBalance = DashboardPage.getBalanceForCard(0);
-        var result = secondBalance > firstBalance;
-
-        Assertions.assertTrue(result);
-
-    }
 }
